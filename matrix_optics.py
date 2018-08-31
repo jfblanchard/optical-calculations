de5f62450c113fc38bc610c190e490b5962fdf69
jfblanchard <jonflintblanchard@gmail.com>
2018-08-30 22:45:26 -0400

    Updated to use classes

--- matrix_optics.py
+++ matrix_optics.py
@@ -16,45 +16,94 @@ For now, launch only a marginal and chief ray
 Todo: Put in the determinant check.
 
 
+
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def optical_ray(y,u):
-    """ Define a ray for matrix optical calculations. 
-        Todo: make this construct a ray grid and think about the best structure
-        to store lots of rays (ex: millions)"""
+class OpticalRay:
+    
+    def __init__(self,y,u):
+        """ Define a ray for matrix optical calculations. 
+            Todo: make this construct a ray grid and think about the best structure
+            to store lots of rays (ex: millions)"""
+            
+        self.yu = np.zeros(shape = (2,1), dtype=float)
+        self.yu[0] = y
+        self.yu[1] = u
+        self.energy = 1.0   
         
-    ray = np.zeros(shape = (2,1), dtype=float)
-    ray[0] = y
-    ray[1] = u
-    
-    return ray
 
-    
-def initialize_system():
-    """Setup structure, creates an empty list.  Use lists for adding, then 
-    convert to numpy arrays for computation.
-    """
-    
-    #initialize rays
-    start_rays = []  
-    start_rays.append(optical_ray(0,.1))   #chief and marginal rays
-    start_rays.append(optical_ray(.1,0))
-    
-    for i in range(10):  #make a ray fan
-        start_rays.append(optical_ray(0.0,i/10.0 - .5))
-    
-    #initialize system list 
-    system = []     
-    
-    #initialize locationations
-    location = [0]
 
-    return start_rays, system, location
+class OpticalSystem:
     
+    def __init__(self):
+        """Setup creates an empty list structure for rays, locations, and 
+        system matricies
+        """
+        
+        #initialize rays - at least get an empty list started
+        self.start_rays = [] 
+        self.locations = [0]  #new list of locations
+        
+        #start_rays.append(optical_ray(0,.1))   #chief and marginal rays
+        #start_rays.append(optical_ray(.1,0))
+        
+        #initialize and store system matricies list 
+        self.matricies= []     
+        
+        #stores all the ray parameters
+        self.ray_matrix = np.zeros(1)
+        
+        
+    def create_rayfan(self, ray_height, min_angle, max_angle, num):
+        """ Create a rayfan with extents min_angle to max_angle, with num rays
+        Todo: Add loop for ray_heights too
+        """
+          
+        for i in np.linspace(self, ray_height, min_angle, max_angle, num):  #make a ray fan
+            self.start_rays.append(OpticalRay(ray_height,i)) 
+
+
+    def add_chief_and_marginal_rays(self):
+        # add chief and marginal rays
+        self.start_rays.append(OpticalRay(0,.1))   #need to know height of aperture
+        self.start_rays.append(OpticalRay(.1,0))   #need to know height of object   
+        
+        
+    def add_ray(self,y,u):
+        """ Add an arbitrary ray at height y and angle u
+        """
+        self.start_rays.append(OpticalRay(y,u))
+    
+    
+    def append_matrix(self, M):
+        """Append a matrix to the system.
+        """
+        
+        self.matricies.append(M)
+        
+     
+    def update_system(self):
+        """Progagate rays, recomputes entire system ray matrix
+        """
+        
+        self.ray_matrix = np.zeros((len(self.start_rays),len(self.matricies)+1,2))
+        #load starting rays:
+        count = 0  #need a counter if iterating through objects
+        for i in self.start_rays:
+            self.ray_matrix[count,0,0] = i.yu[0]
+            self.ray_matrix[count,0,1] = i.yu[1]
+            count +=1
+        
+        #multiply by each matrix
+        for i in range(len(self.start_rays)):
+            for j in range(len(self.matricies)):
+                self.ray_matrix[i][j+1] = np.dot(self.matricies[j], self.ray_matrix[i][j])
+        
+            
 
 def xfer_matrix(A,B,C,D):
     """Define a transfer matrix
@@ -91,7 +140,8 @@ def refraction_matrix(n1,n2,radius):
     
 
 def thin_lens_matrix(focal_length):
-    """Matrix to approximate a thin lens.  Input paramter is focal length.
+    """Matrix to approximate a thin lens (no thickenss).  
+    Singular input paramter is focal length.
     """
     
     A = 1
@@ -99,25 +149,10 @@ def thin_lens_matrix(focal_length):
     C = -1/focal_length
     D = 1
     matrix = xfer_matrix(A, B, C, D)    
+    
     return matrix
    
- 
-def update_system(start_rays,system):
-    """Called when the sytem changes, recomputes entire system ray matrix
-    """
-    
-    ray_matrix = np.zeros((len(start_rays),len(system)+1,2))
-    #load starting rays:
-    for i in range(len(start_rays)):
-        ray_matrix[i,0,0] = start_rays[i][0]
-        ray_matrix[i,0,1] = start_rays[i][1]
-    
-    #multiply by each matrix
-    for i in range(len(start_rays)):
-        for j in range(len(system)):
-            ray_matrix[i][j+1] = np.dot(system[j], ray_matrix[i][j])
-    
-    return ray_matrix
+
 
 
 def plot_rays(ray_matx, loc):
@@ -131,32 +166,47 @@ def plot_rays(ray_matx, loc):
     plt.plot(x,y,'--')
     
     for i in range(ray_matx.shape[0]):
-        plt.plot(loc,ray_matx[i,:,0])
-    plt.show()
+        plt.plot(dist,ray_matx[i,:,0])     
         
+    plt.title('Raytrace with Matrix Optics')
+    plt.xlabel('Distance (m)')
+    plt.ylabel('Ray height(m)')
+    plt.show()
+
+
+#def append_element(ele):
+    
 
 
 if __name__ == '__main__':
   
-    start_rays, system, location = initialize_system()
+    system = OpticalSystem()
+    system.add_chief_and_marginal_rays()
+    system.add_ray(.05,.05)
+    
     
     # populate the optical system with matricies
     d1 = xfer_free_space(1)  #create a matrix to propagate 1m
-    system.append(d1)
-    location.append(1)
+    system.matricies.append(d1)
+    system.locations.append(1)
+    
     #system.append(xfer_free_space(1))  #propagate another 1m
     l1 = thin_lens_matrix(.5)  #500mm thin lens
-    system.append(l1)
-    location.append(0)
-    d2 = xfer_free_space(1)  #propagate 1m
-    system.append(d2)
-    location.append(1)
+    system.matricies.append(l1)
+    system.locations.append(0)
     
-    ray_matx = update_system(start_rays,system)
-    cum_loc = np.cumsum(location)
-    plot_rays(ray_matx,cum_loc)
+    d2 = xfer_free_space(1)  #propagate 1m
+    system.matricies.append(d2)
+    system.locations.append(1)
+    
+    system.update_system()
+    ray_matx = system.ray_matrix
+    dist = np.cumsum(system.locations)
+    plot_rays(ray_matx,dist)
 
     print('final ray parameters: \n', str(ray_matx))
     
+    # Tested with a .5m lens, correct in that a infintie image goes to zero
+    # at 1m away, an on-axis point object is collimated, and a 
     
     
